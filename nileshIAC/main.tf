507a9af1fee67a242f9b219c5340162cb729c8a4
NileshKishore <nileshkishore2001@gmail.com>
2024-06-28 12:29:06 +0530

    initial

--- nileshIAC/main.tf
+++ nileshIAC/main.tf
@@ -1,10 +1,17 @@
+# terraform {
+#  backend "gcs" {
+#    bucket  = "BUCKET_NAME"
+#    prefix  = "terraform/state"
+#  }
+# }
+
 provider "google" {
   project = "nileshfirst"
   region  = "asia-south1"
 }
 
 resource "google_storage_bucket" "bucket" {
-  name     = "mybygcptyc"
+  name     = "backend_bucket"
   location = "asia-south1"
   force_destroy = true
 }
