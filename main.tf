0703deac0875fefa80e0f2c83bb8eee47be98491
NileshKishore <nileshkishore2001@gmail.com>
2024-06-28 12:08:16 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = "nileshfirst"
+  region  = "asia-south1"
+}
+resource "google_storage_bucket" "bucket" {
+  name     = "mybygcptyc"
+  location = "asia-south1"
+  force_destroy = true
+}
