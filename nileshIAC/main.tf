1343d2c7e24034703d8dcd0a1b7e47e4984c7d70
NileshKishore <nileshkishore2001@gmail.com>
2024-06-28 13:08:20 +0530

    initial

--- nileshIAC/main.tf
+++ nileshIAC/main.tf
@@ -25,16 +25,16 @@ module "common" {
   subnet_name = local.config.subnet_name
   subnet_cidr_range = local.config.subnet_cidr_range
 }
-module "vendor"{
-  source = "./modules/vendor"
-  bucket_name = local.config.bucket_name
-  region = local.config.region
-  vendor_topic_name = local.config.vendor_topic
-  bigQuery_dataset_id = local.config.bigQuery_dataset_id
-  bigQuery_location = local.config.bigQuery_location
-  bigQuery_table_id = local.config.bigQuery_table_id
-  functionName = local.config.functionName
-  function_entry_point = local.config.function_entry_point
-  project_id = local.config.project_id
-  function_source_file_name = local.config.function_source_file_name
-}
+# module "vendor"{
+#   source = "./modules/vendor"
+#   bucket_name = local.config.bucket_name
+#   region = local.config.region
+#   vendor_topic_name = local.config.vendor_topic
+#   bigQuery_dataset_id = local.config.bigQuery_dataset_id
+#   bigQuery_location = local.config.bigQuery_location
+#   bigQuery_table_id = local.config.bigQuery_table_id
+#   functionName = local.config.functionName
+#   function_entry_point = local.config.function_entry_point
+#   project_id = local.config.project_id
+#   function_source_file_name = local.config.function_source_file_name
+# }
